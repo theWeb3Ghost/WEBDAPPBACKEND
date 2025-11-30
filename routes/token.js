1a7f6393267bc42b316125ac58893368f8117855
David <116164652+theWeb3Ghost@users.noreply.github.com>
2025-11-30 13:33:46 +0100

    Refactor token routes to use updated controller methods

--- routes/token.js
+++ routes/token.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { claimTokens, sendTokens,getBalance } from "../controllers/tokencontroller.js";
+import { claim, send,getBalance } from "../controllers/tokencontroller.js";
 import verify from "../middleware/verifyGoogle.js";  // <-- FIXED (no require)
 
 const router = express.Router();
 
-router.post("/claim", verify, claimTokens);
-router.post("/send", verify, sendTokens);
+router.post("/claim", verify, claim);
+router.post("/send", verify, send);
 router.get("/balance/:address", getBalance);
 
 export default router;   
